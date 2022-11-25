5db1ede82e80a882b89f210406ad179ef81cac01
Alexey Ryabov <lesha_12_01@mail.ru>
2022-11-25 23:36:55 +0300

    feat: implement functions for RSVP system

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -15,4 +15,133 @@ contract Web3RSVP {
     }
 
     mapping(bytes32 => CreateEvent) public idToEvent;
+
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID
+    ) external {
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+    }
+
+    function createNewRSVP(bytes32 eventId) external payable {
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        require(msg.value == myEvent.deposit, "NOT ENOUGH");
+
+        require(block.timestamp <= myEvent.eventTimestamp, "ALREADY HAPPENED");
+
+        require(
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
+            "This event has reached capacity"
+        );
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(
+                myEvent.confirmedRSVPs[i] != msg.sender,
+                "ALREADY CONFIRMED"
+            );
+        }
+
+        myEvent.confirmedRSVPs.push(payable(msg.sender));
+    }
+
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        require(myEvent.paidOut == false, "ALREADY PAID OUT");
+
+        address rsvpConfirm;
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            address confirmedRSVP = myEvent.confirmedRSVPs[i];
+
+            if (confirmedRSVP == attendee) {
+                rsvpConfirm = confirmedRSVP;
+                break;
+            }
+        }
+
+        require(rsvpConfirm == attendee, "NO RSVP TO CONFIRM");
+
+        for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
+        }
+
+        // Mark as claimed RSVP before sending to avoid reentrancy attack.
+        myEvent.claimedRSVPs.push(attendee);
+
+        (bool sent, ) = attendee.call{value: myEvent.deposit}("");
+
+        if (!sent) {
+            myEvent.claimedRSVPs.pop();
+        }
+
+        require(sent, "Failed to send Ether");
+    }
+
+    function confirmAllAttendees(bytes32 eventId) external {
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    function withdrawUnclaimedDeposits(bytes32 eventId) external {
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        require(!myEvent.paidOut, "ALREADY PAID");
+
+        require(
+            block.timestamp >= (myEvent.eventTimestamp + 7 days),
+            "TOO EARLY"
+        );
+
+        uint256 unclaimed = myEvent.confirmedRSVPs.length -
+            myEvent.claimedRSVPs.length;
+
+        uint256 payout = unclaimed * myEvent.deposit;
+
+        // Mark as paid before sending to avoid reentrancy attack.
+        myEvent.paidOut = true;
+
+        (bool sent, ) = msg.sender.call{value: payout}("");
+
+        if (!sent) {
+            myEvent.paidOut = false;
+        }
+
+        require(sent, "Failed to send Ether");
+    }
 }
