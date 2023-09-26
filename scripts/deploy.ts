7d71ae2b1301b674085bfec5548dbe89bed68987
Alexey Ryabov <lesha_12_01@mail.ru>
2023-09-26 18:09:36 +0300

    chore: add deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const rsvpContractFactory = await ethers.getContractFactory("Web3RSVP");
+  const rsvpContract = await rsvpContractFactory.deploy();
+
+  await rsvpContract.deployed();
+  console.log("Contract deployed to:", rsvpContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
